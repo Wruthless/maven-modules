bdf6306506a994e1271d34fadb2e0ac2ed93bbed
Wruthless <wsgnzls@comcast.net>
2023-04-10 15:35:49 -0400

    Using jaxb to generate a Java class via a module.

--- web-app/src/main/java/org/wruthless/controllers/Main.java
+++ web-app/src/main/java/org/wruthless/controllers/Main.java
@@ -1,4 +1,4 @@
-package org.wruthless;
+package org.wruthless.controllers;
 
 public class Main {
     public static void main(String[] args) {
---
+++ web-app/src/main/java/org/wruthless/controllers/UserController.java
@@ -0,0 +1,15 @@
+package org.wruthless.controllers;
+
+
+import org.wruthless.domain.UserCommand;
+import org.wruthless.entities.User;
+
+public class UserController {
+
+    User saveUser(UserCommand command) {
+
+        // fake impl
+        return new User();
+    }
+
+}
