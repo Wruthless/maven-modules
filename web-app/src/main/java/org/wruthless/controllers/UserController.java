f4367b604b0a525c4b432f109054e3ff7d358937
Wruthless <wsgnzls@comcast.net>
2023-04-12 11:49:41 -0400

    Updated UserController and removed generated class.

--- web-app/src/main/java/org/wruthless/controllers/UserController.java
+++ web-app/src/main/java/org/wruthless/controllers/UserController.java
@@ -1,6 +1,7 @@
 package org.wruthless.controllers;
 
 
+import org.wruthless.converters.UserMapper;
 import org.wruthless.domain.UserCommand;
 import org.wruthless.entities.User;
 
@@ -9,7 +10,8 @@ public class UserController {
     User saveUser(UserCommand command) {
 
         // fake impl
-        return new User();
+        //return new User();
+        return UserMapper.INSTANCE.userCommandToUser(command);
     }
 
 }
